660cb3504dcdf18da6d4a96e1bfeb6e602d0ea6f
Akram-00 <aliakram9789@gmail.com>
2025-06-24 00:45:18 +0530

    rate limiter added

--- src/presentation/routes/imageUpload.route.ts
+++ src/presentation/routes/imageUpload.route.ts
@@ -14,7 +14,7 @@ const publicUploadLimiter = rateLimit({
 // Multer setup (in-memory)
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 20 * 1024 * 1024 }, // 5MB
   fileFilter: (req, file, cb) => {
     const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
     if (!allowedTypes.includes(file.mimetype)) {
@@ -32,6 +32,7 @@ const imageUploadRouter = Router();
  */
 imageUploadRouter.post(
   "/upload",
+  publicUploadLimiter,
   upload.single("file"), // Multer middleware to handle file upload
   ImageUploadController.uploadImage
 );
